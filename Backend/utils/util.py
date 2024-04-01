4c7ab8ce418175125e5c0970bd0007296cf20a10
jasonsalem2002 <jason.salem19@hotmail.com>
2024-04-01 20:00:20 +0300

    Added utils for token functions

---
+++ Backend/utils/util.py
@@ -0,0 +1,26 @@
+import datetime, jwt
+from config import SECRET_KEY
+
+def create_token(user_id):
+    payload = {
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=4),
+        'iat': datetime.datetime.utcnow(),
+        'sub': user_id
+    }
+    return jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+def extract_auth_token(authenticated_request):
+    auth_header = authenticated_request.headers.get('Authorization')
+    if auth_header:
+        return auth_header.split(" ")[1]
+    else:
+        return None
+
+def decode_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, 'HS256')
+        return payload['sub']
+    except jwt.ExpiredSignatureError:
+        raise jwt.ExpiredSignatureError("Token expired")
+    except jwt.InvalidTokenError:
+        raise jwt.InvalidTokenError("Invalid token")
