48ba3cd3c61b519d58e189f469292549e427c0d7
jasonsalem2002 <jason.salem19@hotmail.com>
2024-04-01 19:47:45 +0300

    Added Transaction model and schema

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+ma = Marshmallow(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ Backend/schemas/transactionSchema.py
@@ -0,0 +1,8 @@
+from app import ma
+
+class TransactionSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "usd_amount", "lbp_amount", "usd_to_lbp", "added_date", "user_id")
+
+transaction_schema = TransactionSchema()
+transactions_schema = TransactionSchema(many=True)
