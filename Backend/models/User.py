a99a319d2ef2aefd1fff81141344b84316817a2e
jasonsalem2002 <jason.salem19@hotmail.com>
2024-04-01 19:50:57 +0300

    Added user model and schema

---
+++ Backend/models/User.py
@@ -0,0 +1,10 @@
+from app import db, bcrypt
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(30), unique=True)
+    hashed_password = db.Column(db.String(128))
+
+    def __init__(self, user_name, password):
+        self.user_name = user_name
+        self.hashed_password = bcrypt.generate_password_hash(password)
---
+++ Backend/schemas/userSchema.py
@@ -0,0 +1,7 @@
+from app import ma
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "user_name")
+
+user_schema = UserSchema()
