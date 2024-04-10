608cb2d3bb1b6ad926ad6d474d8e5adce0d1755f
Eric Njeim <epn01@mail.aub.edu>
2024-04-10 20:40:05 +0300

    Added MPAndroidChart:v3.1.0' as a dependancy in app level gradle, replaced mavenCentral() by jcenter() and added  maven { url 'https://jitpack.io' } in settings.gradle , tested linechart by adding dummy points,experimented attributes to make sure we use perfect layout

--- Android/exchange-android-EricNJ/app/src/main/java/com/example/currencyexchange/ChartFragment.kt
+++ Android/exchange-android-EricNJ/app/src/main/java/com/example/currencyexchange/ChartFragment.kt
@@ -5,10 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
 
-
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 class ChartFragment : Fragment() {
 
+    private lateinit var linechart1: LineChart
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,8 +26,51 @@ class ChartFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chart, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_chart, container, false)
+        linechart1 = view.findViewById(R.id.chart1)
+        val description = Description().apply {
+            text = "Exchange Rate"
+            setPosition(150f, 15f)
+        }
+        linechart1.apply {
+            setDescription(description)
+            axisRight.isEnabled = false // Disables right Y-Axis
+        }
+
+        val xValues = listOf("One", "Two", "Three", "Four")
+        linechart1.xAxis.apply {
+            position = XAxis.XAxisPosition.BOTTOM
+            valueFormatter = IndexAxisValueFormatter(xValues)
+            labelCount = 4
+            textSize=15f
+            granularity = 1f
+        }
+
+        linechart1.axisLeft.apply {
+            axisMinimum = 88f
+            axisMaximum = 90f
+            axisLineWidth = 2f
+            textSize=15f
+            // axisLineColor = Color.BLACK
+            labelCount = 10
+        }
+
+        val revenueComp1 = arrayListOf(88f, 89f, 90f, 88.6f)
+        val entries1 = revenueComp1.mapIndexed { index, value ->
+            Entry(index.toFloat(), value)
+        }
+
+        val dataSet = LineDataSet(entries1, "Revenue").apply {
+            //color = Color.BLUE
+            //valueTextColor = Color.BLACK
+            lineWidth = 2f
+        }
+
+        linechart1.data = LineData(dataSet)
+        linechart1.invalidate() // Refreshes the chart
+
+        return view
     }
 
-    }
+
+}
