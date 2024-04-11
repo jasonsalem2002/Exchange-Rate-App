a469ebaf1bdedcaa23f30df91743cac04e497b19
jasonsalem2002 <jason.salem19@hotmail.com>
2024-04-11 21:51:51 +0300

    Blocked OPTIONS method completely

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
@@ -40,6 +40,11 @@ def create_app():
     app.register_blueprint(chat_bp)
     app.register_blueprint(usernames_bp)
     app.register_blueprint(group_bp)
+
+    @app.before_request
+    def block_options():
+        if request.method == 'OPTIONS':
+            return jsonify({'error': 'Method Not Allowed'}), 405
 
     return app
 
