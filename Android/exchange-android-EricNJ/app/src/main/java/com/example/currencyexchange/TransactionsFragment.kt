5a283b09207c198207a4246045565495f558466a
Eric Njeim <epn01@mail.aub.edu>
2024-04-27 18:56:03 +0300

    set up stats xml file

--- Android/exchange-android-EricNJ/app/src/main/java/com/example/currencyexchange/TransactionsFragment.kt
+++ Android/exchange-android-EricNJ/app/src/main/java/com/example/currencyexchange/TransactionsFragment.kt
@@ -1,6 +1,7 @@
 package com.example.currencyexchange
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -81,11 +82,12 @@ class TransactionsFragment : Fragment() {
                         call: Call<List<Transaction>>,
                         response: Response<List<Transaction>>
                     ) {
+
                         if(response.body()==null){
                             Toast.makeText(requireContext(), "No transactions found.", Toast.LENGTH_LONG).show()
                         }
                         else{
-                        transactions?.addAll(response.body()!!)
+                            transactions?.addAll(response.body()!!)
                         adapter?.notifyDataSetChanged()
                     }}
                 })
